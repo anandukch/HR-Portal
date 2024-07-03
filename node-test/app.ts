5626d360c4b09cc85d96746f0950bbf5dd224cb8
Anandu <anandukch@gmail.com>
2024-07-03 14:20:03 +0530

    middleware

--- node-test/app.ts
+++ node-test/app.ts
@@ -1,9 +1,13 @@
 import { Request, Response } from "express";
 import express from "express";
 import employeeRouter from "./employeeRouter";
+import bodyParser from "body-parser";
+import { loggerMiddleWare } from "./loggerMiddleware";
 
 const server = express();
+server.use(bodyParser.json())
 
+server.use(loggerMiddleWare)
 server.get("/", (req: Request, res: Response) => {
     console.log(req.url);
     res.status(200).send("Hello world");
--- node-test/employeeRouter.ts
+++ node-test/employeeRouter.ts
@@ -1,27 +1,63 @@
 import { Router, Request, Response } from "express";
+import Employee from "./Employee";
 const employeeRouter = Router();
 
+let employees: Employee[] = [
+    {
+        id: 1,
+        name: "Annadu",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: 2,
+        name: "Adarsh",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+];
+
 employeeRouter.get("/", (req: Request, res: Response) => {
-    console.log(req.url);
-    res.status(200).send("Get all employees");
+    res.status(200).send(employees);
 });
 
 employeeRouter.get("/:id", (req: Request, res: Response) => {
-    console.log(req.url);
-    res.status(200).send("Get an employee");
+    const employeeId = Number(req.params.id);
+    const employee = employees.find((employee) => employee.id == employeeId);
+    res.status(200).send(employee);
 });
 
 employeeRouter.put("/:id", (req: Request, res: Response) => {
-    console.log(req.url);
+    console.log(req.body);
+
+    employees = employees.map((employee) => {
+        if (employee.id == Number(req.params.id)) {
+            return req.body;
+        }
+        return employee;
+    });
     res.status(200).send("Updated an employee");
 });
 
-employeeRouter.post("/", (req, res) => {
-    res.status(201).send("Employee created");
+employeeRouter.post("/", (req: Request, res: Response) => {
+    const body = req.body;
+    const newId = employees.length + 1;
+    const employee: Employee = {
+        id: newId,
+        name: body.name,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    };
+    employees.push(employee);
+    res.status(201).send(employee);
 });
 
-employeeRouter.delete("/:id", (req, res) => {
-    res.status(204).send("Employee deleted");
+employeeRouter.delete("/:id", (req: Request, res: Response) => {
+    const employeeId = Number(req.params.id);
+    // employees = employees.filter((employee) => employee.id != employeeId);
+    const idx = employees.findIndex((employee) => employee.id == employeeId);
+    employees.splice(idx, 1);
+    res.status(200).send(employees);
 });
 
 export default employeeRouter;
